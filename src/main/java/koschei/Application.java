20cd511871031de2134b598b74dba03e51df14c1
Vladislav A <validator1@yandex.ru>
2021-09-13 20:50:07 +0300

    change url in config

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("--------- VL main() -------");
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         KoscheiTheDeathless koscheiTheDeathless =
